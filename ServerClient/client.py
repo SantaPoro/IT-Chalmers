c45bec7346a7bd1729b643b8d40c0ec3b83b889e
SantaPoro <31341766+SantaPoro@users.noreply.github.com>
2018-08-28 10:41:52 +0200

    Add files via upload
    uppdated clients

--- ServerClient/client.py
+++ ServerClient/client.py
@@ -1,9 +1,22 @@
+from multiprocessing import Process
+
 import zmq
 import random
 import sys
 import time
+import sys
 
 port = "5556"
+if len(sys.argv) > 1:
+    port = sys.argv[1]
+
+
+name = input("Enter username: ")
+
+
+print("Connecting to port: %s" % port)
+
+
 context = zmq.Context()
 socket = context.socket(zmq.PAIR)
 socket.connect("tcp://localhost:%s" % port)
@@ -11,12 +24,12 @@ socket.connect("tcp://localhost:%s" % port)
 while True:
     msg = socket.recv()
     print(msg)
-
     send_this = input(":")
 
     if send_this == 'q':
+        socket.send_string("QUIT")
         break
 
-    socket.send_string("client message: %s" % send_this)
+    socket.send_string("%s: %s" % (name, send_this))
 
     time.sleep(1)
--- ServerClient/server.py
+++ ServerClient/server.py
@@ -1,17 +1,62 @@
+from multiprocessing import Process
+
 import zmq
 import random
 import sys
 import time
 import multiprocessing
+import threading
+
+
+def take_input():
+    print("Take_Input")
+    while True:
+        input("Skriv: ")
+
+
+class MyThread(threading.Thread):
+    def __init__(self, threadID, name, counter):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.counter = counter
+
+    def run(self):
+        print("Starting " + self.name)
+
+        while True:
+            print("Running: " + self.name)
+            take_input()
+
+        # print("Exiting " + self.name)
+
+
+thread1 = MyThread(1, "Thread take input", 1)
+thread1.start()
 
 port = "5556"
+if len(sys.argv) > 1:
+    port = sys.argv[1]
+
+print("Listening to port: %s" % port)
+
+
+def main_loop():
+    print("main_loop")
+    while True:
+        socket.send_string("Server message to client3")
+        msg = socket.recv().decode()
+        if msg == "QUIT":
+            print("Quitting server!")
+            break
+
+        print(msg)
+        time.sleep(1)
+
+
 context = zmq.Context()
 socket = context.socket(zmq.PAIR)
 socket.bind("tcp://*:%s" % port)
 
 
-while True:
-    socket.send_string("Server message to client3")
-    msg = socket.recv()
-    print(msg)
-    time.sleep(1)
+main_loop()
